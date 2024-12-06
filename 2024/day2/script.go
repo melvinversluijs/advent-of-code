dd7c3f5181060ae11b13135b65c984f947537954
Melvin Versluijs <melvin.versluijs@returnless.com>
2024-12-06 10:46:19 +0100

    Implement 2024 - Day 2 - P2

--- 2024/day2/script.go
+++ 2024/day2/script.go
@@ -22,46 +22,53 @@ func main() {
 
 	dataSet := string(dataSetInBytes[:])
 
-	safeReportsCount := calculateSafeReportsCount(dataSet)
+	safeReportsCount, safeReportsCountWithDampener := calculateSafeReportsCount(dataSet)
 	fmt.Println("Safe reports count:", safeReportsCount)
+	fmt.Println("Safe reports count with dampener:", safeReportsCountWithDampener)
 }
 
-func calculateSafeReportsCount(dataSet string) int {
+func calculateSafeReportsCount(dataSet string) (int, int) {
 	var total int = 0
+	var totalWithDampener = 0
 
 	reports := strings.Split(dataSet, "\n")
 	for _, report := range reports {
-		if isSafeReport(report) {
+		if report == "" {
+			continue
+		}
+
+		values := convertStringSliceToIntSlice(strings.Split(report, " "))
+
+		// If there are less than 2 items in the report, the report can be skipped
+		if len(values) < 2 {
+			continue
+		}
+
+		if isSafeReport(values) {
 			total++
+			continue
+		}
+
+		if isSafeWithDampener(values) {
+			totalWithDampener++
 		}
 	}
 
-	return total
+	return total, total + totalWithDampener
 }
 
-func isSafeReport(report string) bool {
-	if report == "" {
-		return false
-	}
-
-	values := convertStringSliceToIntSlice(strings.Split(report, " "))
-
-	// If there are less than 2 items in the report, they are safe
-	if len(values) < 2 {
-		return true
-	}
-
-	valuesDescending := slices.Clone(values)
-	sort.Sort(sort.Reverse(sort.IntSlice(valuesDescending)))
+func isSafeReport(report []int) bool {
+	reportDescending := slices.Clone(report)
+	sort.Sort(sort.Reverse(sort.IntSlice(reportDescending)))
 
 	// If the values are not the same as the ascending or descending sorted values it is a false report.
-	if !slices.IsSorted(values) && !slices.Equal(values, valuesDescending) {
+	if !slices.IsSorted(report) && !slices.Equal(report, reportDescending) {
 		return false
 	}
 
 	// Check that the steps between the values are at least 1 and max 3.
-	for i := 1; i < len(valuesDescending); i++ {
-		step := valuesDescending[i-1] - valuesDescending[i]
+	for i := 1; i < len(reportDescending); i++ {
+		step := reportDescending[i-1] - reportDescending[i]
 
 		if step < 1 || step > 3 {
 			return false
@@ -69,6 +76,20 @@ func isSafeReport(report string) bool {
 	}
 
 	return true
+}
+
+func isSafeWithDampener(report []int) bool {
+	for index := range report {
+		clonedReport := slices.Clone(report)
+		dampenedReport := append(clonedReport[:index], clonedReport[index+1:]...)
+
+		// If a dampened version is safe, return true.
+		if isSafeReport(dampenedReport) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func convertStringSliceToIntSlice(stringSlice []string) []int {
