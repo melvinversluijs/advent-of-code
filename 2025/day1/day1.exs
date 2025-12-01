30f355d36e2048c2bb73aa3f783310afc73df102
Melvin Versluijs <melvin.versluijs@returnless.com>
2025-12-01 19:18:12 +0100

    2025 - Day 1 - Part 2

--- 2025/day1/day1.exs
+++ 2025/day1/day1.exs
@@ -4,14 +4,14 @@ defmodule Day1 do
     @maximum_point 100
 
     def exercise1 do
-        input = File.read!("./input.txt")
+      {_, zero_hits} = get_input()
+        |> Enum.reduce({@starting_point, 0}, fn line, {current_point, zero_hits} ->
+          <<direction::binary-size(1), numbers::binary>> = line
+          points = String.to_integer(numbers)
 
-        {_, zero_hits} = input
-          |> String.split("\n", trim: true)
-          |> Enum.reduce({@starting_point, 0}, fn line, {current_point, zero_hits} ->
-            <<direction::binary-size(1), numbers::binary>> = line
-            points = String.to_integer(numbers)
-
+          if points === 0 do
+            {current_point, zero_hits}
+          else
             # The points can be more than the range permits.
             # But we only need the remainder of the division by the maximum.
             points = rem(points, @maximum_point)
@@ -23,12 +23,47 @@ defmodule Day1 do
               else: zero_hits
 
             {new_point, zero_hits}
-          end)
+          end
+        end)
 
-          IO.puts(zero_hits)
+      IO.puts("Part 1: #{zero_hits}")
     end
 
-    defp move("L", points, current_point) do
+    def exercise2 do
+      {_, zero_hits} = get_input()
+        |> Enum.reduce({@starting_point, 0}, fn line, {current_point, zero_hits} ->
+          <<direction::binary-size(1), numbers::binary>> = line
+          points = String.to_integer(numbers)
+
+          # Calculate how many times it went past 0, and add it to the zero_hits.
+          zero_hits = zero_hits + floor(points / @maximum_point)
+
+          # Calculate what the remaining points is.
+          points = rem(points, @maximum_point)
+
+          if points === 0 do
+            # If the points is zero no need to calculate anything else.
+            {current_point, zero_hits}
+          else
+            new_point = move(direction, points, current_point)
+
+            zero_hits = if has_hit_zero?(direction, current_point, new_point),
+              do: zero_hits + 1,
+              else: zero_hits
+
+            {new_point, zero_hits}
+          end
+        end)
+
+      IO.puts("Part 2: #{zero_hits}")
+    end
+
+    defp get_input() do
+      File.read!("./input.txt")
+        |> String.split("\n", trim: true)
+    end
+
+    defp move("L", points, current_point) when points <= 100 do
       new_point = current_point - points
 
       # Are now allowed to go below the minumum. Take all that is left from the maximum.
@@ -37,7 +72,7 @@ defmodule Day1 do
         else: new_point
     end
 
-    defp move("R", points, current_point) do
+    defp move("R", points, current_point) when points <= 100 do
       new_point = current_point + points
 
       # Are not allowed to go over the maximum. Take all that is left from the minumum
@@ -45,6 +80,17 @@ defmodule Day1 do
         do: @minimal_point + (new_point - @maximum_point),
         else: new_point
     end
+
+    defp has_hit_zero?(_direction, _current_point, 0), do: true
+
+    defp has_hit_zero?(_direction, 0, _new_point), do: false
+
+    defp has_hit_zero?("L", current_point, new_point),
+      do: new_point > current_point
+
+    defp has_hit_zero?("R", current_point, new_point),
+      do: new_point < current_point
 end
 
-Day1.exercise1()
+Day1.exercise1() # 980
+Day1.exercise2() # 5961
