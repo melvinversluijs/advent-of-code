1bb38b5875c87fe18296d40fc6b4a3a2b7e2ade3
Melvin Versluijs <melvin.versluijs@outlook.com>
2025-12-16 22:06:35 +0100

    2025 - Day 4 - Part 2

--- 2025/day4/day4.exs
+++ 2025/day4/day4.exs
@@ -1,11 +1,16 @@
 defmodule Day4 do
 	def part1 do
-    grid = get_grid()
-
-    liftable_rolls = grid
-      |> Enum.count(fn {{x, y}, char} -> can_lift_roll(grid, x, y, char) end)
+    liftable_rolls = get_grid()
+      |> lift_one_round()
 
     IO.puts("Result Part 1: #{liftable_rolls}")
+	end
+
+	def part2 do
+		{_grid, liftable_rolls} = get_grid()
+		  |> lift_all_rolls()
+
+		IO.puts("Result Part 2: #{liftable_rolls}")
 	end
 
 	defp get_grid do
@@ -20,6 +25,28 @@ defmodule Day4 do
 			|> Map.new()
 	end
 
+	defp lift_one_round(grid) do
+	  grid
+      |> Enum.count(fn {{x, y}, char} -> can_lift_roll(grid, x, y, char) end)
+	end
+
+	defp lift_all_rolls(grid, total_lifted \\ 0) do
+    {new_grid, lifted_this_round} = lift_one_round_and_remove_from_grid(grid)
+
+		if lifted_this_round > 0,
+		  do: lift_all_rolls(new_grid, total_lifted + lifted_this_round),
+			else: {new_grid, total_lifted}
+	end
+
+	defp lift_one_round_and_remove_from_grid(grid) do
+	  grid
+      |> Enum.reduce({grid, 0}, fn {{x, y}, char}, {new_grid, liftable_rolls} ->
+          if can_lift_roll(new_grid, x, y, char),
+            do: {remove_roll_from_grid(new_grid, {x, y}), liftable_rolls + 1},
+            else: {new_grid, liftable_rolls}
+      end)
+	end
+
 	defp can_lift_roll(_grid, _x, _y, char) when char != "@", do: false
 
 	defp can_lift_roll(grid, x, y, "@") do
@@ -28,7 +55,6 @@ defmodule Day4 do
 
 		neighbour_rolls < 4
 	end
-
 
 	defp get_neighbours(grid, x, y) do
   	[
@@ -39,6 +65,9 @@ defmodule Day4 do
       |> Enum.map(fn pos -> Map.get(grid, pos) end) # Get character at position
 	    |> Enum.reject(&is_nil/1) # Remove non-existent positions
 	end
+
+	defp remove_roll_from_grid(grid, pos), do: Map.put(grid, pos, ".")
 end
 
 Day4.part1() # 1569
+Day4.part2() # 9280
